a35a36f2b54eb66620631a4ba6cf4b40c2d021cf
linkret <leonard.inkret@gmail.com>
2024-10-07 23:32:43 +0200

    Ensure type stability

--- sve_kombinacije.jl
+++ sve_kombinacije.jl
@@ -1,3 +1,4 @@
+using Random
 using JSON
 
 include("cmc24.jl")
@@ -143,7 +144,7 @@ function smjerovi(v::Point,banned_angle_range::Float64 = 0.1)::Vector{IntPoint}
     #return Point[]
 
     susjedi=IntPoint[]
-    used_angles=[]
+    used_angles=Float64[]
     #pretrazi 5 tocki i poslije pospajaj s
     while true
         naj=-5.0
@@ -234,17 +235,17 @@ function searchFrom(v::IntPoint, depth::Int,
         return
     end
 
-    moji_susedi=susedi[v.x,v.y]
-    """
-    moji_susedi=IntPoint[]
-    if (depth >= 3)
-        moji_susedi = susedi[v.x,v.y]
-    else
-        if length(susedi[v.x,v.y]) > 0
-            push!(moji_susedi, rand(susedi[v.x,v.y]))
-        end
-    end
-    """
+    moji_susedi = susedi[v.x,v.y]
+    should_break = false
+    
+    # moji_susedi=IntPoint[]
+    # if depth >= 3
+    #     moji_susedi = susedi[v.x,v.y]
+    # else
+    #     moji_susedi = susedi[v.x,v.y]
+    #     moji_susedi = shuffle(moji_susedi)
+    #     should_break = true
+    # end
     
     for z in moji_susedi
         a=kut_izmedu_tocki(v,z)+π
@@ -260,6 +261,10 @@ function searchFrom(v::IntPoint, depth::Int,
         draw_ray(Point(v.x/10,v.y/10), Point(z.x/10,z.y/10) )
         searchFrom(z, depth+1, a)
         draw_ray(Point(v.x/10,v.y/10), Point(z.x/10,z.y/10) , -1 )
+
+        if should_break
+            break
+        end
     end
 end
 
@@ -388,7 +393,7 @@ while true
     najval[] = 0.0
     println("Searching from $v")
     searchFrom(vint, 0)
-    println("Najbolje do sad: $best[]")
+    println("Najbolje do sad: $(best[])")
 end
 
 
